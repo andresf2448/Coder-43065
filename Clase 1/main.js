352487a32ca0c8d613052493f38b60371c1073ec
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-04-05 20:26:24 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,49 @@
+// comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+//declaracion de una variable
+// let edad = 16; // forma recomendada
+// var nombre = 'andres'; // forma ilegal
+// const DIRECCION = "AV";
+
+// console.log("aca esta el nombre", nombre);
+// console.log("aca esta la edad", edad);
+// console.log("aca esta la direccion", DIRECCION);
+
+// let edad = 0;
+// let nombre = "pepito";
+
+// console.log(typeof(edad));
+// console.log(typeof(nombre));
+
+// nombrePersonaBonito // Camel Case
+// nombre_persona_bonito // Snake Case
+// NombrePersonaBonito //Pascal Case
+
+// let numeroUno = 4;
+// let numeroDos = 1;
+// let suma = numeroUno + numeroDos;
+// let resta = numeroUno - numeroDos;
+// let multiplicacion = numeroUno * numeroDos;
+// let division = numeroUno / numeroDos;
+
+// console.log(suma);
+// console.log(resta);
+// console.log(multiplicacion);
+// console.log(division);
+  
+// let saludo = "Hola";
+// let nombre = "andres";
+// let saludoCompleto = saludo + " " + nombre;
+// console.log(saludoCompleto);
+
+// let nombre = prompt("Ingrese el nombre");
+// alert(nombre);
+
+let numero = parseInt(prompt("Ingrese el numero"));
+alert(typeof(numero))
