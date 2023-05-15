9d291f396396317f000c9f681ac669979cb008a8
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-05-15 18:58:13 -0500

    Clase 11

---
+++ Clase 11/main.js
@@ -0,0 +1,34 @@
+// console.log(localStorage);
+
+// localStorage.setItem("nombre", "pepito");
+// localStorage.setItem("numero", "5");
+// localStorage.setItem("valor", "true");
+
+// let nombre = localStorage.getItem("nombre");
+// console.log(nombre);
+
+// let numero = localStorage.getItem("numero");
+// console.log(numero);
+
+// let valor = localStorage.getItem("valor");
+// console.log(valor);
+
+// sessionStorage.setItem("nombre", "pepito");
+// sessionStorage.setItem("numero", "5");
+// sessionStorage.setItem("valor", "true");
+
+// let nombre = sessionStorage.getItem("nombre");
+// console.log(nombre);
+
+// let numero = sessionStorage.getItem("numero");
+// console.log(numero);
+
+// let valor = sessionStorage.getItem("valor");
+// console.log(valor);
+
+// for (let i = 0; i < localStorage.length; i++) {
+//   let clave = localStorage.key(i);
+
+//   console.log("clave", clave);
+//   console.log("valor", localStorage.getItem(clave));
+// }
